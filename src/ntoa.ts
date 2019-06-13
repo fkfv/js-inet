5c57f55e34b00d76b4d80983fb6d7b9cba0de83c
fkfv <20618646+fkfv@users.noreply.github.com>
2019-06-13 15:53:07 +1200

    Add safety checks to ntop4() function

--- src/ntoa.ts
+++ src/ntoa.ts
@@ -1,7 +1,13 @@
-const ntop4 = function (addr: Buffer): string {
-  return Array.from(addr).join('.')
-}
+const ntop4 = function (addr: Buffer): string|null {
+  const parts = Array.from(addr);
+
+  if (parts.length !== 4) {
+    return null;
+  }
+
+  return parts.join('.');
+};
 
 export {
-  ntop4,
-}
+  ntop4
+};
